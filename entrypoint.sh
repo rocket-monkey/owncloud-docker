668a3783f0eab48be6ad54e35585e0103acd42d7
Matti Rita-Kasari <matti.rita-kasari@jubic.fi>
2015-11-28 05:56:11 +0200

    - User configurable ownCloud. - Fix nginx config.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+envsubst < /workdir/passwd.template > /tmp/passwd
+export LD_PRELOAD=libnss_wrapper.so
+export NSS_WRAPPER_PASSWD=/tmp/passwd
+export NSS_WRAPPER_GROUP=/etc/group
+
 #/bin/bash
 exec "/usr/bin/supervisord"
 #exec /workdir/php5-fpm.sh
